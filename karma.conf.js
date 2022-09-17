45ed3f2068bb7d952693b2f9086b0fefba41f98c
theNoobCoder123 <patelhrishikesh2000@gmail.com>
2022-09-18 02:58:51 +0530

    update: renamed to chatscoop

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/sendit'),
+      dir: require('path').join(__dirname, './coverage/chatscoop'),
       subdir: '.',
       reporters: [
         { type: 'html' },
